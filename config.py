cac6f9d3cb1d30b1b13b9dd1aa1efe6117cc22c9
kataya1 <mohamedkataya1@gmail.com>
2020-05-10 00:07:34 +0200

    after env

--- config.py
+++ config.py
@@ -7,7 +7,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
+dialect = 'postgres'
 
+username = 'postgres'
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+password = 'postgres'
+
+host = 'localhost'
+
+port = '5432'
+
+db_name = 'fyyur'
+
+#if there is no password there won't be ':' in the link 
+if password:
+    password = ':' + password
+
+#  IMPLEMENT DATABASE URL
+SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}{password}@{host}:{port}/{db_name}'
